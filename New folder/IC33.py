a9ba02149449c7d4d06dccef32cdb444d6dac8b4
Paul Charles <santosh41690@gmail.com>
2018-02-03 03:29:22 -0600

    commit

---
+++ New folder/IC33.py
@@ -0,0 +1,13 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+
+link ='https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India'
+hi = urllib.request.urlopen(link)
+parsedCode = BeautifulSoup(urllib.request.urlopen(link),"html.parser")
+print(parsedCode.prettify())
+print(parsedCode.title)
+aTags = parsedCode.find_all('a')
+print(aTags)
+for i in aTags:
+    print(i.get('href'))
