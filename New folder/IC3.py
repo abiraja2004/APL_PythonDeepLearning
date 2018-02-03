03eedf8b9a8ab2121183254e259cd27820f7f8e4
Paul Charles <santosh41690@gmail.com>
2018-02-03 01:50:05 -0600

    changes

---
+++ New folder/IC3.py
@@ -0,0 +1,14 @@
+#sentence = input('Give a sentence: ')
+sentence = 'hello world this is my first hello'
+sent = sentence.split()
+mydict={}
+for i in sent:
+    if i in mydict:
+        val = mydict.get(i,"none")
+        mydict[i]=val+1
+    else:
+        mydict[i]=1
+
+
+print(mydict)
+
